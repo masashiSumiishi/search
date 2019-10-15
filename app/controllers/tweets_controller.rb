d8b75d37c1129a1acd00f2f4e2c54190514a4d10
Masashi <t15519ms@gmail.com>
2019-10-15 23:43:40 +0900

    Complete tweet search

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+before_action :twitter_client, only: [:search]
+
+  def search
+    @tweets = []
+    if params[:keyword].present?
+      tweets = @client.search(params[:keyword], count: 10, result_type: "recent").take(10)
+      tweets.each do |tw|
+        @tweets << tw.text
+      end
+    end
+  end
+
+  def twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_consumer_key
+      config.consumer_secret     = Rails.application.secrets.twitter_consumer_secret
+      config.access_token        = Rails.application.secrets.twitter_access_token
+      config.access_token_secret = Rails.application.secrets.twitter_access_token_secret
+    end
+  end
+end
